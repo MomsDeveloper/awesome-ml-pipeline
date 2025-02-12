321b97e2ae9e5a7f4d12aa1e45800c6689183d4c
MomsDeveloper <georgiy.khabner@mail.ru>
2025-02-12 12:36:36 +0300

    start training classification models

---
+++ utils/pipeline.py
@@ -0,0 +1,39 @@
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+
+
+def build_pipeline(X):
+    # Define features
+    num_features = X.select_dtypes(include=['int64', 'float64']).columns
+    cat_features = X.select_dtypes(include=['object']).columns
+
+    def pipe(model):
+        # Define transfomations 
+        num_transformer = Pipeline([
+            ('imputer', SimpleImputer(strategy='median')),
+            ('scaler', StandardScaler())
+        ])
+
+        cat_transformer = Pipeline([
+            ('imputer', SimpleImputer(strategy='most_frequent')),
+            ('onehot', OneHotEncoder(handle_unknown='ignore'))
+        ])
+
+        # Define preprocessor
+        preprocessor = ColumnTransformer([
+            ('num', num_transformer, num_features),
+            ('cat', cat_transformer, cat_features)
+        ])
+
+        # Define model pipeline
+        pipeline = Pipeline([
+            ('preprocessor', preprocessor),
+            ('model', model)
+        ])
+
+        return pipeline
+
+    return pipe
+
